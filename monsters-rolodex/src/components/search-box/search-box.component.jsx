50c7e54879ee015a7a0504ba725e7612f8ea5b90
Gaurav Mahadik <gaurav1896.gm@gmail.com>
2021-04-07 16:18:14 -0700

    Adding Searchbox Component and Styling

---
+++ monsters-rolodex/src/components/search-box/search-box.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './search-box.styles.css';
+
+export const SearchBox = props => (
+    <input 
+    className = 'search'
+    type='search' placeholder='Search Monsters'
+    onChange={props.onSearchChange}
+    />
+)
